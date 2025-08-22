d357d065c88ce59b5ca34e22c260b550e5498eaf
Michael Yang <themichaelyang@gmail.com>
2025-08-22 13:20:55 -0400

    add 2017 day 12 part 2

--- 2017/12/12.rb
+++ 2017/12/12.rb
@@ -5,14 +5,17 @@ end
 
 class Node < Struct.new(:neighbors, :visited); end
 
-def part_1
-  input = read_input('input')
-  indexed_nodes = input.map do |line|
+def make_indexed_nodes(input)
+  input.map do |line|
     neighbors_str = line.split(' <-> ').last
     Node.new(neighbors_str.split(', ').map(&:to_i), false)
   end
+end
 
-  puts bfs(0, indexed_nodes).length
+def part_1
+  input = read_input('input')
+  indexed_nodes = make_indexed_nodes(input)
+  bfs(0, indexed_nodes).length
 end
 
 def bfs(start, indexed_nodes)
@@ -22,7 +25,7 @@ def bfs(start, indexed_nodes)
   while !queue.empty?
     current_index = queue.shift
     current = indexed_nodes[current_index]
-    
+
     if !current.visited
       reachable.append(current_index)
       current.visited = true
@@ -40,4 +43,20 @@ def bfs(start, indexed_nodes)
   reachable
 end
 
-part_1
+def part_2
+  input = read_input('input')
+  indexed_nodes = make_indexed_nodes(input)
+  groups = []
+  
+  0.upto(indexed_nodes.length - 1) do |i|
+    start = indexed_nodes[i]
+    if !start.visited
+      groups.append(bfs(i, indexed_nodes))
+    end
+  end
+
+  groups.length
+end
+
+puts part_1
+puts part_2
