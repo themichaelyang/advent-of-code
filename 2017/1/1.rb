279aa7b55bcc7d1a6019cc58044602e572de20e9
Michael Yang <themichaelyang@gmail.com>
2025-09-12 23:54:50 -0400

    add 2017 day 1 part 2

--- 2017/1/1.rb
+++ 2017/1/1.rb
@@ -8,4 +8,18 @@ def part_1
   arr.append(arr.last).each_cons(2).map(&:uniq).select {|cons| cons.length == 1}.sum {|val| val.first}
 end
 
+def part_2
+  arr = read_input
+  arr.each_with_index.sum do |v, i|
+    check = (i + arr.length / 2) % arr.length
+
+    if arr[i] == arr[check]
+      arr[i]
+    else
+      0
+    end
+  end
+end
+
 p part_1
+p part_2
