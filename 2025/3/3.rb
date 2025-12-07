44b1fc5b5f5965c5e2ab600d3c4e883b489e4a5e
Michael Yang <themichaelyang@gmail.com>
2025-12-07 03:25:47 -0800

    add 2025 day 3 part 2

--- 2025/3/3.rb
+++ 2025/3/3.rb
@@ -16,6 +16,22 @@ module Advent2025::Day3
       end
     end
   end
+
+  class Part2
+    def self.solve
+      input = File.open('input').read
+      input.split.sum do |line|
+        i = -1
+        12.times.map do |place|
+          digit = line[(i + 1)..(-12 + place)]&.chars&.max  #: as String
+          i = line.index(digit, i + 1)                      #: as Integer
+
+          digit
+        end.join.to_i
+      end
+    end
+  end
 end
 
 puts Advent2025::Day3::Part1.solve
+puts Advent2025::Day3::Part2.solve
