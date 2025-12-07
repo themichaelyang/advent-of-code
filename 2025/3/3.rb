e719c93da4e201b92771d9a7f611b790f2b86842
Michael Yang <themichaelyang@gmail.com>
2025-12-07 03:14:24 -0800

    add 2025 day 3 part 1

---
+++ 2025/3/3.rb
@@ -0,0 +1,21 @@
+# typed: true
+
+module Advent2025; end
+
+module Advent2025::Day3
+  include Kernel
+
+  class Part1
+    def self.solve
+      input = File.open('input').read
+      input.split.sum do |line|
+        tens = line[0..-2]&.chars&.max     #: as String
+        i = line.index(tens)               #: as Integer
+        ones = line[(i + 1)..]&.chars&.max #: as String
+        (tens + ones).to_i
+      end
+    end
+  end
+end
+
+puts Advent2025::Day3::Part1.solve
