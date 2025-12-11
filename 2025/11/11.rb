a29b8828ccb401a3c882672e9b14cb914fa75c21
Michael Yang <themichaelyang@gmail.com>
2025-12-10 23:35:27 -0800

    add 2025 day 11 part 2

--- 2025/11/11.rb
+++ 2025/11/11.rb
@@ -20,29 +20,35 @@ module Advent2025::Day11
     nodes
   end
 
-
   class Part1
     extend Advent2025::Day11
 
-    def self.paths(from, to, nodes)
+    def self.count_paths(from, to, nodes)
       return 1 if from == to
-      nodes[from].sum {|n| paths(n, to, nodes)}
+      nodes[from].sum {|n| count_paths(n, to, nodes)}
     end
 
     def self.solve
       nodes = parse(File.open('input').read)
-      paths('you', 'out', nodes)
+      count_paths('you', 'out', nodes)
     end
   end
 
   class Part2
     extend Advent2025::Day11
 
+    def self.count_paths_with(from, to, required, nodes, memoized={})
+      return (required.empty? ? 1 : 0) if from == to
+      return memoized[[from, required]] if memoized.include?([from, required])
+      memoized[[from, required]] ||= nodes[from].sum {|n| count_paths_with(n, to, required - [from], nodes, memoized)}
+    end
+
     def self.solve
-      input = File.open('example').read
+      nodes = parse(File.open('input').read)
+      count_paths_with('svr', 'out', ['dac', 'fft'], nodes)
     end
   end
 end
 
 p Advent2025::Day11::Part1.solve
-# p Advent2025::Day11::Part2.solve
+p Advent2025::Day11::Part2.solve
